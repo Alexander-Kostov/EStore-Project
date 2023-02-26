ea13dd9625e81a9beba08c2d114b5af1eb46980d
Alexander <alex3747@abv.bg>
2023-02-26 18:58:20 +0200

    setting up the project

---
+++ Supermarket-Project/src/test/java/com/example/SupermarketProject/SupermarketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupermarketProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
