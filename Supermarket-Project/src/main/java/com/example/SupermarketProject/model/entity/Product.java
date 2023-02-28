707ad3933795d8745482c922ae2cdff72075e841
Alexander <alex3747@abv.bg>
2023-02-28 20:21:58 +0200

    Adding first entities

---
+++ Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/Product.java
@@ -0,0 +1,28 @@
+package com.example.SupermarketProject.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private LocalDateTime expiryDate;
+
+
+
+
+
+}
---
+++ Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.SupermarketProject.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany
+    private List<Product> products;
+
+
+}
