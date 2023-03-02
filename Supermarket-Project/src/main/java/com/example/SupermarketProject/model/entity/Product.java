c446849fcfb779f71798feb714b774e5f5ba1a43
Alexander <alex3747@abv.bg>
2023-03-02 11:41:01 +0200

    Adding templates, css, js

--- Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/Product.java
+++ Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/Product.java
@@ -22,7 +22,48 @@ public class Product {
     private LocalDateTime expiryDate;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public Product setId(Long id) {
+        this.id = id;
+        return this;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Product setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Product setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public Product setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+        return this;
+    }
 }
--- Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/User.java
+++ Supermarket-Project/src/main/java/com/example/SupermarketProject/model/entity/User.java
@@ -14,10 +14,55 @@ public class User {
 
     private String username;
 
+    private String email;
+
     private String password;
 
     @OneToMany
-    private List<Product> products;
+    private List<Product> basket;
 
+    public Long getId() {
+        return id;
+    }
 
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public List<Product> getBasket() {
+        return basket;
+    }
+
+    public User setBasket(List<Product> basket) {
+        this.basket = basket;
+        return this;
+    }
 }
