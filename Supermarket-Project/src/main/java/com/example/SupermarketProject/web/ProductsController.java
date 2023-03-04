d99f0396bf7853caca969f84a6351db3e69d3616
Alexander <alex3747@abv.bg>
2023-03-04 14:12:15 +0200

    fixing nav.bar

---
+++ Supermarket-Project/src/main/java/com/example/SupermarketProject/web/ProductsController.java
@@ -0,0 +1,46 @@
+package com.example.SupermarketProject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductsController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+
+        return "product-list";
+    }
+
+    @GetMapping("/products-details")
+    public String productsDetail() {
+
+        return "product-detail";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+
+        return "cart";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout() {
+
+        return "checkout";
+    }
+
+    @GetMapping("/my-account")
+    public String myAccount() {
+
+        return "my-account";
+    }
+
+
+}
+
